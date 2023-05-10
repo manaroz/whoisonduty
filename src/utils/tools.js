2933f2f483b09cd921efca5cf5cb6f5048385cfa
manaroz <manaroz@protonmail.com>
2023-05-10 21:27:59 +0200

    zakomitowane

---
+++ src/utils/tools.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text } from 'react-native'
+
+export const MainLogo = () => (
+    <Text
+        style={{
+            fontFamily:'Sofia-Regular',
+            color:'#db3eb1',
+            textDecorationLine:'underline',
+            textDecorationColor:'#4146e6',
+            fontSize:40
+        }}
+    >
+        Who is on duty ?
+    </Text>
+)
